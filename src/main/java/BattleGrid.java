97d179a86d4034bb6cfe86202ee1abae095263f7
mckennaS26 <mstraup@student-bmhs-la.org>
2026-03-11 11:43:37 -0700

    test

--- src/main/java/BattleGrid.java
+++ src/main/java/BattleGrid.java
@@ -1,53 +1,70 @@
 public class BattleGrid {
     // TODO: Declare fields here
-    //   private Unit[][] grid
-    //   private int rows
-    //   private int cols
+    private Unit[][] grid;
+    private int rows;
+    private int cols;
 
     // TODO: Write the constructor
-    //   public BattleGrid(int rows, int cols)
+    public BattleGrid(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        grid = new Unit[rows][cols];
+
+    }
     //   - Set rows and cols from parameters
     //   - Initialize grid as new Unit[rows][cols]
     //   - All cells start as null (empty) automatically
 
     // TODO: Write getters
-    //   public int getRows()
-    //   public int getCols()
+    public int getRows() {
+        return rows;
+    }
+    public int getCols() {
+        return cols;
+    }
 
     // TODO: Write placeUnit(int row, int col, Unit unit)
-    //   public boolean placeUnit(int row, int col, Unit unit)
+    public boolean placeUnit(int row, int col, Unit unit) {
+        if(unit == null) {
+            return false;
+        }
+        else if(row > rows || col > cols) {
+            return false;
+        }
+        else if()
+    }
     //   - Return false (do nothing) if unit is null
     //   - Return false (do nothing) if row or col is out of bounds
     //   - Return false (do nothing) if the cell is already occupied
     //   - Otherwise: place unit at grid[row][col] and return true
 
     // TODO: Write getUnit(int row, int col)
-    //   public Unit getUnit(int row, int col)
+    public Unit getUnit(int row, int col)
     //   - Return null if row or col is out of bounds
     //   - Otherwise: return grid[row][col]  (may be null if cell is empty)
 
     // TODO: Write countUnitsInRow(int row)
-    //   public int countUnitsInRow(int row)
+    public int countUnitsInRow(int row)
     //   - Return 0 if row is out of bounds
     //   - Otherwise: traverse the row; count and return non-null cells
 
     // TODO: Write countUnitsInCol(int col)
-    //   public int countUnitsInCol(int col)
+    public int countUnitsInCol(int col)
     //   - Return 0 if col is out of bounds
     //   - Otherwise: traverse the column; count and return non-null cells
 
     // TODO: Write strengthInRow(int row)
-    //   public int strengthInRow(int row)
+    public int strengthInRow(int row)
     //   - Return 0 if row is out of bounds
     //   - Otherwise: traverse the row; sum and return strength values of non-null units
 
     // TODO: Write strengthInCol(int col)
-    //   public int strengthInCol(int col)
+    public int strengthInCol(int col)
     //   - Return 0 if col is out of bounds
     //   - Otherwise: traverse the column; sum and return strength values of non-null units
 
     // TODO: Write getStrongestRow()
-    //   public int getStrongestRow()
+    public int getStrongestRow()
     //   - Return the index of the row with the highest total strength
     //   - If all rows have equal strength (including all zeros), return 0
     //   - In case of a tie, return the lowest row index
--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,19 +1,32 @@
 public class Unit {
     // TODO: Declare fields here
-    private String name
-    //   private char symbol
-    //   private int strength
+    private String name;
+    private char symbol;
+    private int strength;
 
     // TODO: Write the constructor
-    //   public Unit(String name, char symbol, int strength)
+    public Unit(String name, char symbol, int strength) {
+        this.name = name;
+        this.symbol = symbol;
+        this.strength = strength;
+    }
     //   - Set all fields from parameters
 
     // TODO: Write getters
-    //   public String getName()
-    //   public char getSymbol()
-    //   public int getStrength()
+    public String getName() {
+        return name;
+    }
+    public char getSymbol() {
+        return symbol;
+    }
+    public int getStrength() {
+        return strength;
+    }
 
     // TODO: Write toString()
+    public String toString() {
+        return name + "(" + symbol + "," + strength + ")";
+    }
     //   Returns: "Archer(A,3)"
     //   Format:  name + "(" + symbol + "," + strength + ")"
 }
